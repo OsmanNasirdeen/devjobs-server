2e47913eec8051875aa5db81b5a20919a1e33f7a
osman nasirdeen <nasirdeenosman@gmail.com>
2023-05-17 08:10:54 +0000

    data added

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+const data = require("./data.json");
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.status(201).json({ success: true, data: data });
 });
 
 app.listen(port, () => {

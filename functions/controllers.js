d8203e209d2e9a6469e081575fc55fdbd27e4deb
osman nasirdeen <nasirdeenosman@gmail.com>
2023-06-11 15:59:15 +0000

    more routes added

--- functions/controllers.js
+++ functions/controllers.js
@@ -5,11 +5,11 @@ const getAllData = (req, res) => {
   res.status(201).json({ success: true, length: data.length, data: data });
 };
 
-const getSpecificData = (req, res) => {
+const getDataByName = (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
   const { categoryName } = req.params;
   const filteredData = data.filter((item) => {
-    // uppercase of categoryName to allow case-insensitive includes search
+    // uppercase both variables to allow case-insensitive includes search
     const upperCategoryName = categoryName.toUpperCase();
     return item.position.toUpperCase().includes(upperCategoryName);
   });
@@ -17,28 +17,99 @@ const getSpecificData = (req, res) => {
     .status(201)
     .json({ success: true, length: filteredData.length, data: filteredData });
 };
-
-const getDataByNameLocation = (req, res) => {
+const getDataByLocation = (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
-  const { position, location } = req.query;
-  if (!position || !location) {
-    return res
-      .status(201)
-      .json({ success: true, length: data.length, data: data });
-  }
+  const { location } = req.params;
   const filteredData = data.filter((item) => {
-    // uppercase both to allow case-insensitive includes search
-    const upperPosition = position.toUpperCase();
-    const upperLocation = location.toUpperCase();
-    return (
-      item.position.toUpperCase().includes(upperPosition) &&
-      item.location.toUpperCase() === upperLocation
-    );
+    return item.location.toUpperCase() === location.toUpperCase();
   });
-
   res
     .status(201)
     .json({ success: true, length: filteredData.length, data: filteredData });
 };
 
-module.exports = { getAllData, getSpecificData, getDataByNameLocation };
+const getDataByContract = (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  const { contract } = req.params;
+  const filteredData = data.filter((item) => {
+    return item.contract.toUpperCase() === contract.toUpperCase();
+  });
+  res
+    .status(201)
+    .json({ success: true, length: filteredData.length, data: filteredData });
+};
+
+const getDataByQueryStrings = (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  const { position, location, contract } = req.query;
+
+  if (!position && !location && !contract) {
+    return res
+      .status(201)
+      .json({ success: true, length: data.length, data: data });
+  }
+
+  if (position && !location && contract) {
+    const upperPosition = position.toUpperCase();
+    const upperContract = contract.toUpperCase();
+    const filteredData = data.filter((item) => {
+      return (
+        item.position.toUpperCase().includes(upperPosition) &&
+        item.contract.toUpperCase() === upperContract
+      );
+    });
+    return res
+      .status(201)
+      .json({ success: true, length: filteredData.length, data: filteredData });
+  }
+  if (position && location && !contract) {
+    const upperPosition = position.toUpperCase();
+    const upperLocation = location.toUpperCase();
+    const filteredData = data.filter((item) => {
+      return (
+        item.position.toUpperCase().includes(upperPosition) &&
+        item.location.toUpperCase() === upperLocation
+      );
+    });
+    return res
+      .status(201)
+      .json({ success: true, length: filteredData.length, data: filteredData });
+  }
+  // filter for location and contract
+  if (!position && location && contract) {
+    const upperLocation = location.toUpperCase();
+    const filteredData = data.filter((item) => {
+      return (
+        item.contract.toUpperCase() === contract.toUpperCase() &&
+        item.location.toUpperCase() === upperLocation
+      );
+    });
+    return res
+      .status(201)
+      .json({ success: true, length: filteredData.length, data: filteredData });
+  }
+  // if all params are provided
+  if (position && location && contract) {
+    const filteredData = data.filter((item) => {
+      const upperPosition = position.toUpperCase();
+      const upperLocation = location.toUpperCase();
+      const upperContract = contract.toUpperCase();
+      return (
+        item.position.toUpperCase().includes(upperPosition) &&
+        item.location.toUpperCase() === upperLocation &&
+        item.contract.toUpperCase() === upperContract
+      );
+    });
+    res
+      .status(201)
+      .json({ success: true, length: filteredData.length, data: filteredData });
+  }
+};
+
+module.exports = {
+  getAllData,
+  getDataByName,
+  getDataByLocation,
+  getDataByContract,
+  getDataByQueryStrings,
+};
--- index.js
+++ index.js
@@ -3,14 +3,22 @@ const app = express();
 const port = 8000;
 const {
   getAllData,
-  getSpecificData,
-  getDataByNameLocation,
+  getDataByName,
+  getDataByLocation,
+  getDataByContract,
+  getDataByQueryStrings,
 } = require("./functions/controllers");
+
 app.get("/", getAllData);
 
-app.get("/positions", getDataByNameLocation);
+app.get("/positions", getDataByQueryStrings);
 
-app.get("/category/:categoryName", getSpecificData);
+app.get("/category/:categoryName", getDataByName);
+app.get("/locations/:location", getDataByLocation);
+app.get("/contracts/:contract", getDataByContract);
+app.get("*", (req, res) => {
+  res.status(404).send("ROUTE NOT FOUND!");
+});
 app.listen(port, () => {
   console.log(`server is listening at port ${port}`);
 });
